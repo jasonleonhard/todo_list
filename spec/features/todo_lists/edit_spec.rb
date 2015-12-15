56b6ac06bcea2a6c943f65499fa5c8009b99e255
un5t0ppab13 <devbrights@gmail.com>
2015-12-15 11:36:31 -0800

    edit_spec.rb with rspec feature that creates a todo_list for each before and assign to variable

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,19 +1,32 @@
 require "spec_helper"
 
 describe "Editing todo lists" do
-  it "updates a todo list successfully with correct info" do
-    todo_list = TodoList.create(title: "Groceries", description: "Grocery list")
+  # rspec feature that creates a todo_list for each before and assign to variable
+  let!(:todo_list){ TodoList.create(title: "Groceries", description: "Grocery list") }
+
+  def update_todo_list(options={})
+    options[:title] ||= "Example title"
+    options[:description] ||= "Example description"
+    todo_list = options[:todo_list]
     visit "/todo_lists"
     within "#todo_list_#{todo_list.id}" do
       click_link "Edit"
     end
-    fill_in "Title", with: "New title"
-    fill_in "Description", with: "New description"
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
     click_button "Update Todo list"
-    
+  end
+
+  it "updates a todo list successfully with correct information" do
+    update_todo_list todo_list: todo_list, 
+                     title: "New title", 
+                     description: "New description"
+
     todo_list.reload
+    
     expect(page).to have_content("Todo list was successfully updated")
     expect(todo_list.title).to eq("New title")
     expect(todo_list.description).to eq("New description")
   end
 end
+# bin/rspec spec/features/todo_lists/edit_spec.rb
