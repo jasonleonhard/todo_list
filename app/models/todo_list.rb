65e03992bedb4e6605fddb4749b2022adfc9d499
un5t0ppab13 <devbrights@gmail.com>
2015-12-15 09:36:35 -0800

    create_spec and validations

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,4 @@
 class TodoList < ActiveRecord::Base
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,16 +1,28 @@
 require 'spec_helper'
 require 'capybara/rspec'
 
-
 describe "Creating todo lists" do
   it "redirects to the todo list index page on success" do
     visit "/todo_lists"
     click_link "New Todo list"
     expect(page).to have_content("New Todo List")
-        
     fill_in "Title", with: "Example todo list"
     fill_in "Description", with: "This is what I have to say for now"
     click_button "Create Todo list"
     expect(page).to have_content("Todo list was successfully created.")
+  end
+
+  it "displays error when no title" do
+    expect(TodoList.count).to eq(0)
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+    fill_in "Title", with: ""
+    fill_in "Description", with: "bla bla bla"
+    click_button "Create Todo list"
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+    visit "/todo_lists"
+    expect(page).to_not have_content("bla bla bla")
   end
 end
