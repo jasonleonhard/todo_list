84fd50c609e01cc6ca6fd359e24269de69cc479b
un5t0ppab13 <devbrights@gmail.com>
2015-12-15 12:10:13 -0800

    edit_spec.rb improved

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "Editing todo lists" do
-  # rspec feature that creates a todo_list for each before and assign to variable
+
   let!(:todo_list){ TodoList.create(title: "Groceries", description: "Grocery list") }
 
   def update_todo_list(options={})
@@ -21,12 +21,45 @@ describe "Editing todo lists" do
     update_todo_list todo_list: todo_list, 
                      title: "New title", 
                      description: "New description"
-
     todo_list.reload
-    
     expect(page).to have_content("Todo list was successfully updated")
     expect(todo_list.title).to eq("New title")
     expect(todo_list.description).to eq("New description")
   end
+
+  it "displays an error with title less than 3 characters" do
+    update_todo_list todo_list: todo_list, title: "hi"
+    expect(page).to have_content("error")
+    
+    title = todo_list.title
+    todo_list.reload
+    expect(todo_list.title).to eq(title)
+  end
+  
+  it "displays an error with description less than 3 characters" do
+    update_todo_list todo_list: todo_list, description: "hi"
+    expect(page).to have_content("error")
+    
+    description = todo_list.description
+    todo_list.reload
+    expect(todo_list.description).to eq(description)
+  end
+
+  it "displays an error with no title" do
+    update_todo_list todo_list: todo_list, title: ""
+    expect(page).to have_content("error")
+    
+    title = todo_list.title
+    todo_list.reload
+    expect(todo_list.title).to eq(title)
+  end
+
+  it "displays an error with no description" do
+    update_todo_list todo_list: todo_list, description: ""
+    expect(page).to have_content("error")
+    
+    description = todo_list.description
+    todo_list.reload
+    expect(todo_list.description).to eq(description)
+  end
 end
-# bin/rspec spec/features/todo_lists/edit_spec.rb
